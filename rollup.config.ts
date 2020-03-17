b59a128581ad5777c32de6b7af85608e0a0ace99
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-03-17 13:45:26 +0100

    Switches to different typescript plugin for rollup
    Because the other one raised errors that I couldn't fix:
    
    ```
    src/hyperpath.ts → dist, dist...
    [!] Error: Unexpected token (Note that you need plugins to import files that are not JavaScript)
    src/hyperpath.ts (5:17)
    3:
    4: export class HyperPath {
    5:   createChannel(): Channel {
                        ^
    6:     return new HyperSwarmChannel()
    7:   }
    Error: Unexpected token (Note that you need plugins to import files that are not JavaScript)
        at error (/Users/nlv19703/Documents/Projects/hyperpath/node_modules/rollup/dist/shared/rollup.js:10120:30)
    ```

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
-import typescript from '@rollup/plugin-typescript'
+import typescript from 'rollup-plugin-typescript2'
 import json from '@rollup/plugin-json'
 
 const pkg = require('./package.json')
