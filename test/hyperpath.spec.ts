fd826012e4d9e12ee4779e8a2c2a8d612394681a
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-03-23 09:30:10 +0100

    Refactors tests

--- test/hyperpath.spec.ts
+++ test/hyperpath.spec.ts
@@ -2,31 +2,32 @@ import { HyperPath } from '../src/hyperpath'
 import { Channel } from '../src/channel'
 
 describe('HyperPath', () => {
-  it('can be constructed', () => {
-    expect(new HyperPath()).toBeInstanceOf(HyperPath)
+  let hyperPath: HyperPath
+
+  beforeEach(() => {
+    hyperPath = new HyperPath()
   })
 
   describe('a channel', () => {
-    it('has an id', async () => {
-      const channel = await new HyperPath().createChannel()
+    it('has an auto-generated id', async () => {
+      const channel = await hyperPath.createChannel()
       expect(channel.id).toBeDefined()
+      expect(channel.id.bytes.length).toBe(32)
     })
 
     it('can be created with an explicit id', async () => {
       const id = new Uint8Array(32)
-      const channel = await new HyperPath().createChannel(id)
+      const channel = await hyperPath.createChannel(id)
       expect(channel.id.bytes).toBe(id)
     })
 
     it('cannot be created with too short id', async () => {
       const id = new Uint8Array(31)
-      const hyperPath = new HyperPath()
       await expect(hyperPath.createChannel(id)).rejects.toThrowError(TypeError)
     })
 
     it('cannot be created with too long id', async () => {
       const id = new Uint8Array(33)
-      const hyperPath = new HyperPath()
       await expect(hyperPath.createChannel(id)).rejects.toThrowError(TypeError)
     })
   })
