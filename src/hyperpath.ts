e30e2d0690fd1b2fa970978d8e3f270cbe90ea3c
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-03-24 15:38:14 +0100

    These can be top-level functions

--- src/hyperpath.ts
+++ src/hyperpath.ts
@@ -3,15 +3,13 @@ import { HyperSwarmChannel } from './hyperswarmChannel'
 import { EncryptingChannel } from './encryptingChannel'
 import { randomBytes, secretbox } from 'tweetnacl'
 
-export class HyperPath {
-  createChannel(): Channel {
-    const channelId = ChannelID.createRandom()
-    const key = randomBytes(secretbox.keyLength)
-    return new EncryptingChannel(key, new HyperSwarmChannel(channelId))
-  }
+export function createChannel(): Channel {
+  const channelId = ChannelID.createRandom()
+  const key = randomBytes(secretbox.keyLength)
+  return new EncryptingChannel(key, new HyperSwarmChannel(channelId))
+}
 
-  openChannel(id: Uint8Array, key: Uint8Array): Channel {
-    const channelId = ChannelID.create(id)
-    return new EncryptingChannel(key, new HyperSwarmChannel(channelId))
-  }
+export function openChannel(id: Uint8Array, key: Uint8Array): Channel {
+  const channelId = ChannelID.create(id)
+  return new EncryptingChannel(key, new HyperSwarmChannel(channelId))
 }
--- test/hyperpath.spec.ts
+++ test/hyperpath.spec.ts
@@ -1,16 +1,10 @@
-import { HyperPath } from '../src/hyperpath'
+import { createChannel, openChannel } from '../src/hyperpath'
 import { randomBytes, secretbox } from 'tweetnacl'
 
 describe('HyperPath', () => {
-  let hyperPath: HyperPath
-
-  beforeEach(() => {
-    hyperPath = new HyperPath()
-  })
-
   describe('create new channel', () => {
     it('creates channel with an auto-generated id', () => {
-      const channel = hyperPath.createChannel()
+      const channel = createChannel()
       expect(channel.id).toBeDefined()
       expect(channel.id.bytes.length).toBe(32)
       expect(channel.key?.length).toBe(secretbox.keyLength)
@@ -22,22 +16,18 @@ describe('HyperPath', () => {
     const testKey = randomBytes(secretbox.keyLength)
 
     it('can be opened with an explicit id', () => {
-      const channel = hyperPath.openChannel(id, testKey)
+      const channel = openChannel(id, testKey)
       expect(channel.id.bytes).toBe(id)
     })
 
     it('cannot be opened with invalid id size', () => {
       const invalidId = new Uint8Array(31)
-      expect(() => hyperPath.openChannel(invalidId, testKey)).toThrowError(
-        TypeError
-      )
+      expect(() => openChannel(invalidId, testKey)).toThrowError(TypeError)
     })
 
     it('cannot be opened with invalid key size', () => {
       const invalidKey = new Uint8Array(secretbox.keyLength + 1)
-      expect(() => hyperPath.openChannel(id, invalidKey)).toThrowError(
-        TypeError
-      )
+      expect(() => openChannel(id, invalidKey)).toThrowError(TypeError)
     })
   })
 })
