77846dc7ff89e1bec1ab3f4fec5867e91476139e
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-03-31 08:29:38 +0200

    Setting `libraryTarget` explicitly doesn't seem to be necessary

--- webpack.config.ts
+++ webpack.config.ts
@@ -7,8 +7,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    library: 'hyperpath',
-    libraryTarget: 'global'
+    library: 'hyperpath'
   },
 
   resolve: {
