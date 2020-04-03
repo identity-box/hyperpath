f33aa1211a9b1fff69b03e9869b4aad57e9bf230
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-04-03 09:02:21 +0200

    Adds libp2p bootstrap and webrtc-star packages

---
+++ types/libp2p-bootstrap/index.d.ts
@@ -0,0 +1,3 @@
+export = libp2p_bootstrap
+
+declare class libp2p_bootstrap {}
---
+++ types/libp2p-webrtc-star/index.d.ts
@@ -0,0 +1,5 @@
+export = libp2p_webrtc_star
+
+declare function libp2p_webrtc_star(...args: any[]): any
+
+declare namespace libp2p_webrtc_star {}
