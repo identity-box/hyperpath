ca1b542a494557e6ad6bd8c473414678adf4d158
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-04-07 11:08:50 +0200

    Adds libp2p-secio for experimenting

---
+++ types/libp2p-secio/index.d.ts
@@ -0,0 +1,9 @@
+export const protocol: string
+
+export function secureInbound(localPeer: any, duplex: any, remotePeer: any): any
+
+export function secureOutbound(
+  localPeer: any,
+  duplex: any,
+  remotePeer: any
+): any
