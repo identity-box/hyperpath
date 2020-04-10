185d8e347e92e47c5891abd7a879a0e3f599fe5d
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-04-10 10:26:18 +0200

    Removes some duplicate code

--- test/LibP2PChannel.spec.ts
+++ test/LibP2PChannel.spec.ts
@@ -22,12 +22,16 @@ describe('LibP2PChannel', () => {
     await expect(channel.connect()).resolves.toBe(undefined)
   })
 
-  describe('when listening', () => {
+  describe('connecting', () => {
     let libp2pStub: LibP2PStub
     let channel: LibP2PChannel
     let logSpy: LogSpy
 
-    beforeEach(async () => {
+    beforeEach(() => {
+      logSpy = new LogSpy()
+    })
+
+    function createTestChannel(remoteId?: PeerId) {
       channel = new LibP2PChannel(
         myId,
         channelId,
@@ -35,88 +39,80 @@ describe('LibP2PChannel', () => {
           new Promise(resolve => {
             libp2pStub = new LibP2PStub(options)
             resolve(libp2pStub)
-          })
-      )
-      logSpy = new LogSpy()
-      channel.log = (msg: string, args: any[]) => logSpy.log(msg, args)
-      await channel.connect()
-    })
-
-    it('has configuration with own peer info', () => {
-      expect(libp2pStub.options?.peerInfo?.id).toBe(myId)
-    })
-
-    it('has started', () => {
-      expect(libp2pStub.started).toBeTruthy()
-    })
-
-    it('has added the signalling server multiaddr', () => {
-      const matches = libp2pStub.peerInfo.multiaddrs
-        .toArray()
-        .map(ma => ma.toString())
-        .filter(s => s.includes(signallingServer))
-      expect(matches).toHaveLength(1)
-    })
-
-    it('has registered the hyperpath protocol', () => {
-      expect(libp2pStub.handledProtocol).toBe(protocol)
-    })
-
-    it('has a (just logging for now) handler for that protocol', () => {
-      const handler = libp2pStub.protocolHandler
-      expect(handler).toBeDefined()
-      logSpy.reset()
-      handler!({ stream: 'foo' })
-      expect(logSpy.msg).toBeDefined()
-    })
-  })
-
-  describe('when dialing', () => {
-    let libp2pStub: LibP2PStub
-    let channel: LibP2PChannel
-    let logSpy: LogSpy
-    let remoteId: PeerId
-
-    beforeEach(async () => {
-      remoteId = await PeerId.create({ bits: 512, keyType: 'RSA' })
-      channel = new LibP2PChannel(
-        myId,
-        channelId,
-        () =>
-          new Promise(resolve => {
-            libp2pStub = new LibP2PStub()
-            resolve(libp2pStub)
           }),
         remoteId
       )
-      logSpy = new LogSpy()
       channel.log = (msg: string, args: any[]) => logSpy.log(msg, args)
-      await channel.connect()
+    }
+
+    describe('when listening', () => {
+      beforeEach(async () => {
+        createTestChannel()
+        await channel.connect()
+      })
+
+      it('has configuration with own peer info', () => {
+        expect(libp2pStub.options?.peerInfo?.id).toBe(myId)
+      })
+
+      it('has started', () => {
+        expect(libp2pStub.started).toBeTruthy()
+      })
+
+      it('has added the signalling server multiaddr', () => {
+        const matches = libp2pStub.peerInfo.multiaddrs
+          .toArray()
+          .map(ma => ma.toString())
+          .filter(s => s.includes(signallingServer))
+        expect(matches).toHaveLength(1)
+      })
+
+      it('has registered the hyperpath protocol', () => {
+        expect(libp2pStub.handledProtocol).toBe(protocol)
+      })
+
+      it('has a (just logging for now) handler for that protocol', () => {
+        const handler = libp2pStub.protocolHandler
+        expect(handler).toBeDefined()
+        logSpy.reset()
+        handler!({ stream: 'foo' })
+        expect(logSpy.msg).toBeDefined()
+      })
     })
 
-    it('has started', () => {
-      expect(libp2pStub.started).toBeTruthy()
-    })
+    describe('when dialing', () => {
+      let remoteId: PeerId
 
-    it('has dialed the hyperpath protocol', () => {
-      expect(libp2pStub.dialedProtocols).toContain(protocol)
-    })
+      beforeEach(async () => {
+        remoteId = await PeerId.create({ bits: 512, keyType: 'RSA' })
+        createTestChannel(remoteId)
+        await channel.connect()
+      })
 
-    it('dials a remote with the right peer id', () => {
-      const dialedRemote = libp2pStub.dialedRemote
-      expect(dialedRemote).toBeDefined()
-      expect(dialedRemote?.id).toEqual(remoteId)
-    })
+      it('has started', () => {
+        expect(libp2pStub.started).toBeTruthy()
+      })
 
-    it('has the listener address', () => {
-      const matches = libp2pStub
-        .dialedRemote!.multiaddrs.toArray()
-        .map(ma => ma.toString())
-        .filter(
-          s =>
-            s.includes(signallingServer) && s.includes(remoteId.toB58String())
-        )
-      expect(matches).toHaveLength(1)
+      it('has dialed the hyperpath protocol', () => {
+        expect(libp2pStub.dialedProtocols).toContain(protocol)
+      })
+
+      it('dials a remote with the right peer id', () => {
+        const dialedRemote = libp2pStub.dialedRemote
+        expect(dialedRemote).toBeDefined()
+        expect(dialedRemote?.id).toEqual(remoteId)
+      })
+
+      it('has the listener address', () => {
+        const matches = libp2pStub
+          .dialedRemote!.multiaddrs.toArray()
+          .map(ma => ma.toString())
+          .filter(
+            s =>
+              s.includes(signallingServer) && s.includes(remoteId.toB58String())
+          )
+        expect(matches).toHaveLength(1)
+      })
     })
   })
 })
